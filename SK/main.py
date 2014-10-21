cbe80cb294c41afed1af2e0b44f4e314af5c4134
Nikita Pestrov <ipestrov@gmail.com>
2014-10-21 17:15:19 +0400

    Some user info methods

--- SK/main.py
+++ SK/main.py
@@ -1,13 +1,30 @@
-__author__ = 'denisantyukhov'
+# -*- coding: utf-8 -*-
 import requests
 
 APP_KEY = '3457835747'
 APP_SECRET = '04c6dac1c5eb0226c14e85a50ef9274d'
 ACCESS_TOKEN = '2.00no3HpF_CjAmD38b962df537ZLw8B'
 
+WEIBO_BASE = 'https://api.weibo.com/2/'
+
 def getUsersForKeyword(keyWord):
-    payload = {'client_id': APP_KEY, 'access_token': ACCESS_TOKEN, 'q':keyWord}
-    r = requests.get("https://api.weibo.com/2/search/suggestions/users.json", params=payload)
+    payload = {'access_token':ACCESS_TOKEN,'uid':2003324087}
+    r = requests.get(WEIBO_BASE+"users/show.json", params=payload)
     return r.text
 
-print getUsersForKeyword('Education')
+def getUserActiveFollowers(userId):
+    payload = {'access_token':ACCESS_TOKEN,'uid':userId,'count':200}
+    r = requests.get(WEIBO_BASE+"friendships/followers/active.json", params=payload)
+    return r.text
+
+def getUserFollowersId(userId, startFrom):
+    payload = {'access_token':ACCESS_TOKEN,'uid':userId,'count':5000}
+    r = requests.get(WEIBO_BASE+"friendships/followers/ids.json", params=payload)
+    return r.text
+
+def getUserInfo(userId):
+    payload = {'access_token':ACCESS_TOKEN,'uid':userId}
+    r = requests.get(WEIBO_BASE+"users/show.json", params=payload)
+    return r.text
+
+print getUserInfo(2003324087)

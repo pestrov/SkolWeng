e36a0018a0e39666aebad4fa17675b2a1a97c6ff
Nikita Pestrov <ipestrov@gmail.com>
2014-10-21 14:46:13 +0400

    APP_KEY in place

--- SK/main.py
+++ SK/main.py
@@ -7,8 +7,7 @@ CALLBACK_URL = 'https://github.com/gaphex'
 ACCESS_TOKEN = '2.00no3HpF_CjAmD38b962df537ZLw8B'
 
 def getUsersForKeyword(keyWord):
-    client_id = '3457835747'
-    payload = {'client_id': client_id, 'access_token': ACCESS_TOKEN, 'q':keyWord}
+    payload = {'client_id': APP_KEY, 'access_token': ACCESS_TOKEN, 'q':keyWord}
     r = requests.get("https://api.weibo.com/2/search/suggestions/users.json", params=payload)
     return r.text
 

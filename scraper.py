8322bbf6d12feb52a81960616cfb97c98cbdf252
Kalan <kalan.001@gmail.com>
2014-10-22 14:11:28 +0400

    scraper: intelligent waits

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 import urllib
-
 import time
-
 import re
 
 
@@ -19,10 +21,20 @@ query = u'研究'
 driver = webdriver.Chrome()
 driver.get("http://s.weibo.com/wb/%s&xsort=hot" % urllib.quote(query.encode("utf-8")))
 
-time.sleep(10)
-elems = driver.find_elements_by_css_selector("dd.content")
+#WebDriverWait(driver, 1).until(EC.presence_of_element_located( (By.CLASS_NAME, "global_footer") ))
+while True:
+    try:
+        elems = driver.find_elements_by_css_selector("dd.content")
+        break
+    except:
+        time.sleep(0.1488)
 
 for i in elems:
-    print "user",       digits(i.find_element_by_css_selector("p a").get_attribute('suda-data')[-15:])
-    print "retweets",   digits(i.find_element_by_css_selector("p i+a").text)
-    print "likes",      digits(i.find_element_by_css_selector("p.info a").text)
+    d = {
+        "user":       digits(i.find_element_by_css_selector("p a").get_attribute('suda-data')[-15:]),
+        "retweets":   digits(i.find_element_by_css_selector("p i+a").text),
+        "likes":      digits(i.find_element_by_css_selector("p.info a").text)
+    }
+    print d
+
+driver.close()

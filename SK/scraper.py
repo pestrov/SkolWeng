9ebb44f5f714da98216f9dc0788fe0822efc168a
Kalan <kalan.001@gmail.com>
2014-10-22 15:49:13 +0400

    A good version of scraper

--- SK/scraper.py
+++ SK/scraper.py
@@ -5,31 +5,54 @@ from selenium import webdriver
 import urllib
 import time
 import re
+import sys
 
 
 dstrip = re.compile('[^0-9]+')
 def digits(s):
     return dstrip.sub('', s)
 
-query = u'研究'
+def wait_for(selector):
+    sys.stderr.write("Waiting for %s..." % selector)
+    while True:
+        try:
+            driver.find_element_by_css_selector(selector)
+            break
+        except:
+            time.sleep(0.1488)
+    sys.stderr.write(" done.\n")
 
-driver = webdriver.Chrome()
-driver.get("http://s.weibo.com/wb/%s&xsort=hot" % urllib.quote(query.encode("utf-8")))
+def login(username, password):
+    driver.get("http://weibo.com/login.php")
+    wait_for(".W_login_form .info_list")
+    driver.find_element_by_css_selector(".inp.username input").send_keys(username)
+    driver.find_element_by_css_selector(".inp.password input").send_keys(password)
+    #driver.find_element_by_css_selector(".info_list.login_btn a").click()
+    time.sleep(8)
+    #driver.execute_script("document.getElementsByClassName('login_btn')[1].getElementsByTagName('a')[0].click();")
+    #driver.execute_script("alert('Now click the button.')")
+    #wait_for(".send_weibo")
 
-#WebDriverWait(driver, 1).until(EC.presence_of_element_located( (By.CLASS_NAME, "global_footer") ))
-while True:
-    try:
-        elems = driver.find_elements_by_css_selector("dd.content")
-        break
-    except:
-        time.sleep(0.1488)
+def get_page(query, page=0):
+    driver.get("http://s.weibo.com/wb/%s&xsort=hot" % urllib.quote(query.encode("utf-8")))
 
-for i in elems:
-    d = {
-        "user":       digits(i.find_element_by_css_selector("p a").get_attribute('suda-data')[-15:]),
-        "retweets":   digits(i.find_element_by_css_selector("p i+a").text),
-        "likes":      digits(i.find_element_by_css_selector("p.info a").text)
-    }
-    print d
+    wait_for("dd.content")
+    res = []
+    elems = driver.find_elements_by_css_selector("dd.content")
+    for i in elems:
+        d = {
+            "user":       digits(i.find_element_by_css_selector("p a").get_attribute('suda-data')[-15:]),
+            "retweets":   digits(i.find_element_by_css_selector("p i+a").text),
+            "likes":      digits(i.find_element_by_css_selector("p.info a").text)
+        }
+        res += [d]
 
-driver.close()
+    return res
+
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.set_script_timeout(1)
+    login('ipestrov@gmail.com', '1234Sina1234*')
+    print get_page(u'研究')
+    driver.quit()

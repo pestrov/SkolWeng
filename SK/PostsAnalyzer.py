6a3613c69443520e6e7bed50ab42e3d4417aea0e
gaphex <gaphex@gmail.com>
2014-10-23 21:36:32 +0400

    +iteration over multiple users, write data to file

--- SK/PostsAnalyzer.py
+++ SK/PostsAnalyzer.py
@@ -2,13 +2,17 @@ import json
 json_data=open('uniques.json')
 
 data = json.load(json_data)
-print data.keys()
 
-numberOfGreatPosts = 0
-print len(data)
+
+greatUsers = []
+
 for key, value in data.iteritems():
     if len(value["tag"]) > 1 and value["likes"] >= 5:
-        print value["tag"]
-        numberOfGreatPosts = numberOfGreatPosts + 1
 
-print numberOfGreatPosts
+        greatUsers.append(value["userId"])
+
+
+unique = set(greatUsers)
+print unique
+print len(unique)
+print len(greatUsers)

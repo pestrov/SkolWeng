cff759493195015d6e51aae8c93df3d8cf5840eb
Nikita Pestrov <ipestrov@gmail.com>
2014-10-23 19:11:43 +0400

    Fixed followers links

--- SK/scraper.py
+++ SK/scraper.py
@@ -53,9 +53,9 @@ def get_page(query, max_pages=1):
 def get_relation(uid,mode, max_pages):
     followers = []
     if mode == 'follow':
-         driver.get("http://weibo.com/%s/follow" % uid)
+         driver.get("http://weibo.com/%s/fans" % uid)
     else:
-        driver.get("http://weibo.com/%s/follow?relate=fans" % uid)
+        driver.get("http://weibo.com/%s/fans?relate=follow" % uid)
     wait_for(".S_line1")
     bitchyUid =  digits(driver.find_element_by_css_selector(".S_line1 a").get_attribute("href"))[:-7];
     print bitchyUid
